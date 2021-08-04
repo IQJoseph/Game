a07afa2aa72ed490785565bd99d4ae7302915127
IQJoseph <86134163+IQJoseph@users.noreply.github.com>
2021-08-03 19:01:00 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var a = prompt
+var b = prompt
+function setup() {
+  createCanvas(400, 400);
+
+var button = createButton
+button.mousePressed(swap)
+
+}
+
+function draw() {
+  background(220);
+}
+ function swap()
+ [a,b]= [b,a]
+ console.log("a:", +a)
+console.log("b:", +b)
+
+
